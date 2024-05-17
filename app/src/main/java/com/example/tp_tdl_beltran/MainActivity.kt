f17afc45559fc49b8caa3910a9839e2c4f5aa30b
iborthaburu <iborthaburu@fi.uba.ar>
2024-05-17 19:53:29 -0300

    Prueba piloto

--- app/src/main/java/com/example/tp_tdl_beltran/MainActivity.kt
+++ app/src/main/java/com/example/tp_tdl_beltran/MainActivity.kt
@@ -43,7 +43,8 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
+
+        }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
