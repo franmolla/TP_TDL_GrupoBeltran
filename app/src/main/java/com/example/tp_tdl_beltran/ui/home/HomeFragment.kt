7286085850294d2c1e16db733253f276505bac6e
iborthaburu <iborthaburu@fi.uba.ar>
2024-06-10 01:11:46 -0300

    Branch armando el layout para que aparezcan las aulas habilitadas al momento de cambiar los pickers

--- app/src/main/java/com/example/tp_tdl_beltran/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/tp_tdl_beltran/ui/home/HomeFragment.kt
@@ -1,12 +1,16 @@
 package com.example.tp_tdl_beltran.ui.home
 
+import android.content.Context
+import android.database.Cursor
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.NumberPicker
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -14,6 +18,9 @@ import androidx.navigation.fragment.findNavController
 import com.example.tp_tdl_beltran.R
 import com.example.tp_tdl_beltran.databinding.FragmentHomeBinding
 import com.example.tp_tdl_beltran.ui.shared.SharedViewModel
+import com.google.android.material.button.MaterialButtonToggleGroup
+import android.database.sqlite.SQLiteDatabase
+
 
 class HomeFragment : Fragment() {
 
@@ -39,6 +46,11 @@ class HomeFragment : Fragment() {
         binding.horarioPicker.displayedValues = str
 
         val spinner: Spinner = binding.SpinnerPiso
+        val numberPicker: NumberPicker = binding.horasPicker
+        val textView: TextView = binding.multilineTextView
+        val toggleButtonGroup: MaterialButtonToggleGroup = binding.toggleButtonGroup
+
+
         val pisos = resources.getStringArray(R.array.pisos_array)
         val adapter = ArrayAdapter(
             requireContext(),
@@ -47,6 +59,9 @@ class HomeFragment : Fragment() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
 
+        updateTextView(spinner, numberPicker, toggleButtonGroup, textView)
+
+
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>,
@@ -54,6 +69,9 @@ class HomeFragment : Fragment() {
                 position: Int,
                 id: Long
             ) {
+
+                updateTextView(spinner, numberPicker, toggleButtonGroup, textView)
+
                 Toast.makeText(
                     requireContext(),
                     getString(R.string.selected_item) + " " + pisos[position],
@@ -90,9 +108,35 @@ class HomeFragment : Fragment() {
             }
         }
 
+        numberPicker.setOnValueChangedListener { _, _, _ ->
+            updateTextView(spinner, numberPicker, toggleButtonGroup, textView)
+        }
+
+        toggleButtonGroup.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            if (isChecked) {
+                updateTextView(spinner, numberPicker, toggleButtonGroup, textView)
+            }
+        }
+
         return root
     }
+    private fun updateTextView(spinner: Spinner, numberPicker: NumberPicker, toggleButtonGroup: MaterialButtonToggleGroup, textView: TextView) {
+        val pisoSeleccionado = spinner.selectedItem.toString()
+        val horaSeleccionada = numberPicker.value
+        val selectedButtonId = toggleButtonGroup.checkedButtonId
+        val diaSeleccionado = when (selectedButtonId) {
+            R.id.Lunes -> "Lunes"
+            R.id.Martes -> "Martes"
+            R.id.Miercoles -> "Miércoles"
+            R.id.Jueves -> "Jueves"
+            R.id.Viernes -> "Viernes"
+            R.id.Sabado -> "Sábado"
+            else -> "Ninguno"
+        }
+        val result = "Spinner: $pisoSeleccionado, NumberPicker: $horaSeleccionada, Día: $diaSeleccionado"
+        textView.text = result
 
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
